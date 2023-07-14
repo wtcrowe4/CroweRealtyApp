0235287d71dfb934d4339d738669b08a6537393c
Thomas Crowe <wcrowe@g.clemson.edu>
2023-07-14 06:53:09 -0400

    Starting on models

---
+++ CroweRealtyApp/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweRealtyApp.Models
+{
+    public class Category
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("properties")]
+        public object Properties { get; set; }
+
+    }
+}

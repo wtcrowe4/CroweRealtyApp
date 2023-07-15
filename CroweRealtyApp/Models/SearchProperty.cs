a13842700674257e0e9c5722259cf77233aa819c
Thomas Crowe <wcrowe@g.clemson.edu>
2023-07-15 11:38:04 -0400

    Property models

---
+++ CroweRealtyApp/Models/SearchProperty.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweRealtyApp.Models
+{
+    public class SearchProperty
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("detail")]
+        public string Detail { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        [JsonProperty("isTrending")]
+        public bool IsTrending { get; set; }
+
+        [JsonProperty("categoryId")]
+        public int CategoryId { get; set; }
+
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("bookmarks")]
+        public object Bookmarks { get; set; }
+    }
+}

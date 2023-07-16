32cf15113a973655d43ca16e823ba01d003a2484
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-16 10:39:10 -0400

    Added bookmark model classes

---
+++ CroweRealtyApp/Models/BookmarkList.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweRealtyApp.Models
+{
+    public class BookmarkList
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("status")]
+        public bool Status { get; set; }
+
+        [JsonProperty("user_Id")]
+        public int UserId { get; set; }
+
+        [JsonProperty("propertyId")]
+        public int PropertyId { get; set; }
+    }
+}
--- CroweRealtyApp/Models/Register.cs
+++ CroweRealtyApp/Models/Register.cs
@@ -11,7 +11,8 @@ namespace CroweRealtyApp.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string PasswordConfirm { get; set; }
+        
+        //public string PasswordConfirm { get; set; }
         public string Phone { get; set; }
     }
 }

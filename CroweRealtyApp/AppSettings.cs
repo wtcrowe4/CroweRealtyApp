bb9408c73ad581bcf85bb5d6f12432ca793f3317
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-18 07:02:54 -0400

    Put api url in app settings

---
+++ CroweRealtyApp/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweRealtyApp
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl = "https://localhost:7022/api/";
+    }
+}

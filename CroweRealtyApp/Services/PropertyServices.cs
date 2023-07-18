48116ad9476c3ece836524c5d08f45ec59ab0147
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-18 07:26:27 -0400

    Starting on property services

---
+++ CroweRealtyApp/Services/PropertyServices.cs
@@ -0,0 +1,32 @@
+﻿using CroweRealtyApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweRealtyApp.Services
+{
+    public class PropertyServices
+    {
+        public static async Task<List<Category>> GetCategories()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accesstoken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "categories");
+            return JsonConvert.DeserializeObject<List<Category>>(response);
+        }
+
+        public static async Task<List<TrendingProperty>> GetTrendingProperties()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accesstoken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/Properties/TrendingProperties");
+            return JsonConvert.DeserializeObject<List<TrendingProperty>>(response);
+        }
+
+
+    }
+}

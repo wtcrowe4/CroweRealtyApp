6961157d582cd4df8ceb3c8f4ca1d1828c09830e
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-19 12:38:02 -0400

    Finished property services

--- CroweRealtyApp/Services/PropertyServices.cs
+++ CroweRealtyApp/Services/PropertyServices.cs
@@ -27,6 +27,28 @@ namespace CroweRealtyApp.Services
             return JsonConvert.DeserializeObject<List<TrendingProperty>>(response);
         }
 
+        public static async Task<List<SearchProperty>> FindProperties(string address)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accesstoken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "Properties/FindProperties?address=" + address);
+            return JsonConvert.DeserializeObject<List<SearchProperty>>(response);
+        }
 
+        public static async Task<List<PropertyByCategory>> GetPropertyByCategory(int categoryId)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accesstoken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "Properties/PropertyByCategory?categoryId=" + categoryId);
+            return JsonConvert.DeserializeObject<List<PropertyByCategory>>(response);
+        }
+
+        public static async Task<PropertyDetail> GetPropertyDetail(int propertyId)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", Preferences.Get("accesstoken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "Properties/PropertyDetail?propertyId=" + propertyId);
+            return JsonConvert.DeserializeObject<PropertyDetail>(response);
+        }
     }
 }

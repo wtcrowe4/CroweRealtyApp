e6524aa54b9ea7149fe3ac5074e8c52273ef509e
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-21 07:08:57 -0400

    Register page code

--- CroweRealtyApp/Pages/RegisterPage.xaml.cs
+++ CroweRealtyApp/Pages/RegisterPage.xaml.cs
@@ -1,3 +1,6 @@
+using CroweRealtyApp.Services;
+using CroweRealtyApp.Models;
+
 namespace CroweRealtyApp.Pages;
 
 public partial class RegisterPage : ContentPage
@@ -7,4 +10,22 @@ public partial class RegisterPage : ContentPage
 		InitializeComponent();
 	}
 
+	private async void RegisterButton_Clicked(object sender, EventArgs e)
+	{
+       var response = await UserServices.RegisterUser(FullNameEntry.Text, EmailEntry.Text, PasswordEntry.Text, PhoneEntry.Text);
+		if (response)
+		{
+			await DisplayAlert("Success", "User Registered Successfully", "OK");
+			await Navigation.PushAsync(new LoginPage());
+		} 
+		else
+		{
+			await DisplayAlert("Error", "Something went wrong", "OK");
+		}
+    }
+
+	private async void LoginTapped(object sender, EventArgs e)
+	{
+        await Navigation.PushAsync(new LoginPage());
+    }
 }

e1856e5aec4220639cce7cac36fb122ae175322e
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-22 15:03:09 -0400

    LoginPage code

---
+++ CroweRealtyApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Android.Net;
+using CroweRealtyApp.Services;
+using CroweRealtyApp.Models;
+
+namespace CroweRealtyApp.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void LoginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var response = await UserServices.LoginUser(EmailEntry.Text, PasswordEntry.Text) ;
+            if (response)
+            {
+                Application.Current.MainPage = new CustomTabbedPage();
+            }
+            else
+            {
+                await DisplayAlert("", "Oops something went wrong.", "Cancel");
+            }
+        }
+
+        async void Register_Tapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+        {
+            await Navigation.PushModalAsync(new RegisterPage());
+        }
+    }
+}

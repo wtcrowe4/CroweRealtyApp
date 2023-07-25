d4d79d8e42a75f70ce916b5cf1359f7e3539b9b0
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-25 06:46:57 -0400

    Adding pages, styling login/register

---
+++ CroweRealtyApp/Pages/BookmarksPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CroweRealtyApp.Pages
+{
+    public partial class BookmarksPage : ContentPage
+    {
+        public BookmarksPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CroweRealtyApp/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CroweRealtyApp.Pages
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

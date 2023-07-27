adb49f25bb026b48578669df19c904e8aa3f0c7e
Thomas Crowe <wtcrowe4@outlook.com>
2023-07-27 06:34:26 -0400

    Property list page

--- CroweRealtyApp/Pages/PropertiesListPage.xaml.cs
+++ CroweRealtyApp/Pages/PropertiesListPage.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace CroweRealtyApp.Pages
+﻿using CroweRealtyApp.Models;
+using CroweRealtyApp.Services;
+
+namespace CroweRealtyApp.Pages
 {
     public partial class PropertiesListPage : ContentPage
     {
         public PropertiesListPage(int categoryId, string categoryName)
         {
             InitializeComponent();
+            Title = categoryName;
+            GetPropertiesList(categoryId);
+        }
+
+        private async void GetPropertiesList(int categoryId)
+        {
+            var properties = await PropertyServices.GetPropertyByCategory(categoryId);
+            Properties.ItemsSource = properties;
+        }
+
+        void Properties_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+        {
+            var currentSelection = e.CurrentSelection.FirstOrDefault() as PropertyByCategory;
+            if (currentSelection == null) return;
+            Navigation.PushModalAsync(new PropertyDetailPage(currentSelection.Id));
+            ((CollectionView)sender).SelectedItem = null;
         }
     }
 }
